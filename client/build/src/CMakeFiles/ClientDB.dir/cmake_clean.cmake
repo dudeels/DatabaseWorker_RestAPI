d7de1aa0b23d60a602d4125a103c8fee8d9a8057
Dmitrii Tsarev <dimatsaryov22@gmail.com>
2024-01-14 18:47:32 +0400

    init client, update name in cmake

---
+++ client/build/src/CMakeFiles/ClientDB.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ClientDB.dir/main.cpp.o"
+  "ClientDB"
+  "ClientDB.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ClientDB.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ client/build/src/CMakeFiles/TaskManagerClient.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TaskManagerClient.dir/main.cpp.o"
+  "TaskManagerClient"
+  "TaskManagerClient.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TaskManagerClient.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
